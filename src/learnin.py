b2807de8573550fc404b2a3c4a3a3fc91276420c
root <root@DESKTOP-EH24LUV.localdomain>
2019-06-20 21:14:01 +0200

    second pyautogui added

--- src/learnin.py
+++ src/learnin.py
@@ -1,18 +1,15 @@
-
 from os.path import sep
 import pyautogui, os
-from common_config import TEMP_IMGS,DATASET_IMGS
+from common_config import TEMP_IMGS, DATASET_IMGS
 import cv2
 from matplotlib import pyplot as plt
 import numpy as np
 from win32api import GetSystemMetrics
 
 
-
 def churrete_masivo():
-
     taskbar_icon_to_fg = ("{}{}".format(DATASET_IMGS, "tool_bar_icon.jpg"))
-    img_rgb = cv2.imread("{}{}".format(TEMP_IMGS,"screenshot.png"), cv2.IMREAD_GRAYSCALE)
+    img_rgb = cv2.imread("{}{}".format(TEMP_IMGS, "screenshot.png"), cv2.IMREAD_GRAYSCALE)
     template = cv2.imread(taskbar_icon_to_fg, cv2.IMREAD_GRAYSCALE)
 
     w, h = template.shape[::-1]
@@ -39,72 +36,67 @@ def churrete_masivo():
     cv2.imshow('Detected', img_rgb)
     cv2.waitKey()
 
+
 def refresh_screenshot():
-    im2 = pyautogui.screenshot("{}{}".format(TEMP_IMGS,"screenshot.png"))
+    im2 = pyautogui.screenshot("{}{}".format(TEMP_IMGS, "screenshot.png"))
 
 
 def test():
-
-    template  = ("{}{}".format(DATASET_IMGS, "tool_bar_icon.jpg"))
-    haystack = cv2.imread("{}{}".format(TEMP_IMGS,"screenshot.png"), cv2.IMREAD_COLOR)
+    template = ("{}{}".format(DATASET_IMGS, "tool_bar_icon.jpg"))
+    haystack = cv2.imread("{}{}".format(TEMP_IMGS, "screenshot.png"), cv2.IMREAD_COLOR)
     needle = cv2.imread(template, cv2.IMREAD_COLOR)
 
-    w, h , _= needle.shape
+    w, h, _ = needle.shape
 
     output = needle.copy()
     cv2.namedWindow('output', cv2.WINDOW_NORMAL)
-    cv2.resizeWindow('output', 1024,800)
-    roi=needle[:, 50:]
+    cv2.resizeWindow('output', 1024, 800)
+    roi = needle[:, 50:]
 
     cv2.imshow("output", roi)
-    #cv2.imshow("needle", needle)
+    # cv2.imshow("needle", needle)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-
-
 
     return
 
 
 def testing_img_recongnition_pyautogui():
-    directory= ("{}{}".format(DATASET_IMGS,  "main_window"))
-    print ("directorio de busqueda: {}".format(directory))
+    directory = ("{}{}".format(DATASET_IMGS, "main_window"))
+    print("directorio de busqueda: {}".format(directory))
     for filename in os.listdir(directory):
         if filename.endswith(".asm") or filename.endswith(".py"):
             # print(os.path.join(directory, filename))
             continue
         else:
             template = ("{}{}{}{}".format(DATASET_IMGS, "main_window", sep, filename))
-            print ("buscando template: {}".format(filename))
-            found= pyautogui.locateOnScreen(template)
+            print("buscando template: {}".format(filename))
+            found = pyautogui.locateOnScreen(template)
             if found:
                 left, top, width, height = pyautogui.locateOnScreen(template)
-                print ("left: {} , top: {}, width: {}, height: {}".format(left, top, width, height))
+                print("left: {} , top: {}, width: {}, height: {}".format(left, top, width, height))
             else:
-                print ("template: {} no encontrado".format(template))
-
-
+                print("template: {} no encontrado".format(template))
 
 
 if __name__ == '__main__':
     refresh_screenshot()
-    elemento=None
+    elemento = None
     image_window = "Source Image"
     result_window = "Result window"
-    directory= ("{}{}".format(DATASET_IMGS,  "main_window"))
-    print ("directorio de busqueda: {}".format(directory))
-    img_path =  ("{}{}".format(TEMP_IMGS,"screenshot.png"))
+    directory = ("{}{}".format(DATASET_IMGS, "main_window"))
+    print("directorio de busqueda: {}".format(directory))
+    img_path = ("{}{}".format(TEMP_IMGS, "screenshot.png"))
 
     img = cv2.imread(img_path, cv2.IMREAD_COLOR)
     img_display = img.copy()
 
     for filename in os.listdir(directory):
-        if filename.endswith(".asm") or filename.endswith(".py"):
-            # print(os.path.join(directory, filename))
+        if  filename.endswith(".py"):
             continue
         else:
             template = ("{}{}{}{}".format(DATASET_IMGS, "main_window", sep, filename))
-            print ("buscando template: {}".format(filename))
+            print("buscando template: {}".format(filename))
             templ = cv2.imread(template, cv2.IMREAD_COLOR)
             img_display = img.copy()
 
@@ -112,19 +104,35 @@ if __name__ == '__main__':
             cv2.normalize(result, result, 0, 1, cv2.NORM_MINMAX, -1)
             _minVal, _maxVal, minLoc, maxLoc = cv2.minMaxLoc(result, None)
             matchLoc = maxLoc
+            '''
+            beatiful visual testing purposes
+            
+            #cv2.namedWindow(image_window, cv2.WINDOW_AUTOSIZE)
+            #font = cv2.FONT_HERSHEY_COMPLEX_SMALL
+            #text = '_'.join(filename.split('_')[1:]).split('.')[0]
+            #cv2.putText(img_display, text, (matchLoc[0] + templ.shape[1] - 150, matchLoc[1] + templ.shape[0] - 30),
+            #           font, 1, (0, 255, 0), 1, cv2.LINE_AA)
+            #cv2.rectangle(img_display, matchLoc, (matchLoc[0] + templ.shape[1], matchLoc[1] + templ.shape[0]),
+            #              (0, 0, 0), 2, 8, 0)
+            '''
+            h, w, _ = templ.shape
+            print("h:{}, w:{}".format(h, w))
 
-            cv2.namedWindow(image_window, cv2.WINDOW_AUTOSIZE)
-            cv2.namedWindow(result_window, cv2.WINDOW_AUTOSIZE)
-            cv2.rectangle(img_display, matchLoc, (matchLoc[0] + templ.shape[0], matchLoc[1] + templ.shape[1]), (0, 0, 0),
-                         2, 8, 0)
-            cv2.rectangle(result, matchLoc, (matchLoc[0] + templ.shape[0], matchLoc[1] + templ.shape[1]), (0, 0, 0), 2,
-                         8, 0)
+            center = (int((matchLoc[0] + w / 2)), int((matchLoc[1] + templ.shape[0]) - h / 2))
+            x_center = int(matchLoc[0] + w / 2)
+            y_center= int( (matchLoc[1] + templ.shape[0]) - h/2)
+
+            pyautogui.dragTo(x_center,y_center,  duration=3)
+            '''
+            beatiful visual testing purposes
+            
+            cv2.circle(img_display, center, 5, (0, 255, 0), -1)
             cv2.imshow(image_window, img_display)
-            cv2.imshow(result_window, result)
+
             cv2.waitKey(0)
             cv2.destroyAllWindows()
+            '''
+    # plt.switch_backend('agg')
+    # test()
 
-    #plt.switch_backend('agg')
-    #test()
-
-    #churrete_masivo()
+    # churrete_masivo()
