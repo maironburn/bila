963bc83c69801e6c3ea2aa1d0a728495017bbf58
mario.diaz.rodriguez <Md14z1s0trol>
2019-07-04 14:57:47 +0200

    trying to write

--- src/controllers/interactive_manager.py
+++ src/controllers/interactive_manager.py
@@ -1,6 +1,7 @@
 import os
 import time
 import win32gui, win32con
+import win32api
 import win32ui
 from ctypes import windll
 from PIL import Image
@@ -20,7 +21,7 @@ class Interactive_Manager(object):
     intruder = None
 
     def __init__(self, kw=None):
-        #self.legacy_app = kw.get('legacy', None)
+        # self.legacy_app = kw.get('legacy', None)
         self.window_features()
 
     def window_features(self):
@@ -31,9 +32,13 @@ class Interactive_Manager(object):
         if win32gui.IsWindowVisible(hwnd) and win32gui.GetWindowText(hwnd) != '':
             ''' creates dict window_app: handler '''
             self.running_gui_app.update({"{}".format(win32gui.GetWindowText(hwnd)): hwnd})
-            print("{} -> {}".format(win32gui.GetWindowText(hwnd),win32gui.GetClassName(hwnd)))
-            #print("{} -> {}".format(win32gui.GetWindow(hwnd), win32gui.GetClassName(hwnd)))
+            print("{} -> {}".format(win32gui.GetWindowText(hwnd), win32gui.GetClassName(hwnd)))
+            #if self.dict_windows['main'] in win32gui.GetWindowText(hwnd):
+            if 'texto a escribir' in win32gui.GetWindowText(hwnd):
+                print("{}- > {}".format(self.dict_windows['main'], hwnd))
+                self.legacy_app = hwnd
 
+            # print("{} -> {}".format(win32gui.GetWindow(hwnd), win32gui.GetClassName(hwnd)))
 
     def kill_splash(self):
         for k, v in self.running_gui_app.items():
@@ -86,10 +91,51 @@ class Interactive_Manager(object):
         else:
             raise Exception("Ventana no encontrada")
 
+    def move_mouse(self, x, y):
+
+        win32api.SetCursorPos((x, y))
+        # win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
+        # win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
+
+    # def send_input_hax(self, msg):
+    #
+    #     for c in msg:
+    #         win32api.PostMessage (self.legacy_app, win32con.WM_KEYDOWN, ord(c), 0)
+    #         win32api.PostMessage(self.legacy_app, win32con.WM_CHAR, ord(c), 0)
+    #         win32api.PostMessage(self.legacy_app, win32con.WM_KEYUP, ord(c), 0)
+
+
+    def send_input_hax(self, msg):
+
+        for c in msg:
+            win32api.KeyPress(c)
+
+
+
+    def SetText(self,  text):
+        textformat = win32con.DT_LEFT | win32con.DT_TOP
+        hDC, paintStruct = win32gui.BeginPaint(self.legacy_app)
+
+        rect = win32gui.GetClientRect(self.legacy_app)
+        win32gui.DrawText(
+            hDC,
+            'Hello send by Python via Win32!',
+            -1,
+            rect,
+            win32con.DT_SINGLELINE | win32con.DT_CENTER | win32con.DT_VCENTER)
+
+        win32gui.EndPaint(self.legacy_app, paintStruct)
+        #win32api.SendMessage(self.legacy_app, win32con.WM_SETTEXT, None, text)
+
 
 if __name__ == '__main__':
     # time.sleep(5)
-    #C:\BILA\BILA\N4BL.exe#
+    # C:\BILA\BILA\N4BL.exe#
     wf = Interactive_Manager()
     wf.kill_splash()
+    time.sleep(5)
+    print("q voyyy")
+    #wf.move_mouse(500, 500)
+    wf.send_input_hax('texto a escribirtexto a escribirtexto a escribirtexto a escribir')
+    #wf.SetText('texto a escribir')
     # extract_window_screenshot(dict_windows['main'])
